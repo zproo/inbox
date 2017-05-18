2850b9273e3bb7ebc232bf3bf168e2ccc9254287
zproo <630816477@qq.com>
2017-05-18 22:08:07 +0800

    temp: 文件上传与显示

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 /**
  * Created by zproo on 2017/5/18.
  */
-function route(pathname, handle, response) {
+function route(pathname, handle, response, postData) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        return handle[pathname](response);
+        return handle[pathname](response, postData);
     } else {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write('404 Not Found');
--- server.js
+++ server.js
@@ -6,12 +6,23 @@ var url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = '';
         var pathname = url.parse(request.url).pathname;
         console.log("Request for" + pathname + "  received.");
 
+        request.setEncoding('utf8');
 
-        route(pathname, handle, response);    // index中的回调函数
+        request.addListener('data', function (postDataChunk) {
+            postData += postDataChunk;
+            console.log('recive postDataChunk' + postDataChunk + ',');
 
+        });
+
+        // 接受完成
+        request.addListener('end', function () {
+            // 在数据接受完成时触发路由分发到处理程序，仅触发一次
+            route(pathname, handle, response,postData);    // index中的回调函数
+        });
     }
 
     http.createServer(onRequest).listen(8888);
